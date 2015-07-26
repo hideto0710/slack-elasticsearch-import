004d695aa5ec75e209175796f758b0e81b71328d
hideto0710 <h.inamura0710@gmail.com>
2015-07-26 19:57:52 +0900

    引数を正規表現で検知

--- src/main/scala/util/ArgsUtil.scala
+++ src/main/scala/util/ArgsUtil.scala
@@ -5,19 +5,14 @@ import scala.annotation.tailrec
 object ArgsUtil {
 
   private type OptionMap = Map[Symbol, String]
+  private val argsPattern = """--(.+)""".r
 
-  // TODO: 自動で選択可能な引数を決定したい。
   @tailrec
   def nextOption(list: List[String], map: OptionMap = Map()): OptionMap = list match {
-    case "--from" :: value :: tail =>
-      nextOption(tail, map ++ Map('from -> value))
-    case "--to" :: value :: tail =>
-      nextOption(tail, map ++ Map('to -> value))
-    case "--channel" :: value :: tail =>
-      nextOption(tail, map ++ Map('channel -> value))
+    case argsPattern(p) :: value :: tail =>
+      nextOption(tail, map ++ Map(Symbol(p) -> value))
     case unknownOption :: tail =>
       nextOption(tail, map)
     case _ => map
   }
-
 }
