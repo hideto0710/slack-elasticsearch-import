3bcee767c6debb7738130f38b3b788ebf689591e
hideto0710 <h.inamura0710@gmail.com>
2015-08-12 22:00:50 +0900

    class名変更に合わせて変更

--- src/test/scala/SlackElasticsearchImportSpec.scala
+++ src/test/scala/SlackElasticsearchImportSpec.scala
@@ -21,14 +21,14 @@ class SlackElasticsearchImportSpec(_system: ActorSystem)
 
   private type ImportMap = mutable.Map[String, List[SlackComment]]
 
-  "An ESActor" should "be able to set a new greeting" in {
+  "An ESActor" should "be able to set a new channel and to put comments" in {
     val es = TestActorRef(Props[ESActor])
     val cmt1 = SlackComment(None, Some("cmt_id1"), Some("test"), "test message1", "12345")
     val cmt2 = SlackComment(None, Some("cmt_id2"), Some("test"), "test message2", "12345")
     val cmt3 = SlackComment(None, Some("cmt_id3"), None, "test message3", "12345")
-    es ! ESImportRequest(Channel("channel_id1","channel"), Seq(cmt1))
-    es ! ESImportRequest(Channel("channel_id1","channel"), Seq(cmt2))
-    es ! ESImportRequest(Channel("channel_id2","channel"), Seq(cmt3))
+    es ! SlackFetchResponse(Channel("channel_id1","channel"), Seq(cmt1))
+    es ! SlackFetchResponse(Channel("channel_id1","channel"), Seq(cmt2))
+    es ! SlackFetchResponse(Channel("channel_id2","channel"), Seq(cmt3))
 
     val data: ImportMap = mutable.Map("channel_id1" -> (List(cmt1) ::: List(cmt2)), "channel_id2" -> List(cmt3))
     es.underlyingActor.asInstanceOf[ESActor].data should be(data)
